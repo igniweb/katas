c696e16807d93fb134c2a6eaf682348981514d84
Sébastien Muller <igniweb.net@gmail.com>
2014-08-27 00:28:43 +0200

    Setup and set first kata 'PrimeFactors'

---
+++ readme.md
@@ -0,0 +1,3 @@
+# PRIME FACTORS KATA
+
+Properties of https://github.com/laracasts/Code-Katas-in-PHP
---
+++ src/PrimeFactors.php
@@ -0,0 +1,29 @@
+<?php
+
+class PrimeFactors {
+
+    public function __construct()
+    {
+        //
+    }
+    
+    public function generate($number)
+    {
+        $primes = [];
+
+        $divisor = 2;
+        while ($number > 1)
+        {
+            while (($number % $divisor) === 0)
+            {
+                $primes[] = $divisor;
+                $number  /= $divisor;
+            }
+
+            $divisor++;
+        }
+
+        return $primes;
+    }
+
+}
